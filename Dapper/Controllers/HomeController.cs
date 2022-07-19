f9aea69dd43c2a8931133d9751bf0824c6db944c
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-07-19 10:33:17 +0600

    Add project files.

---
+++ Dapper/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using DapperProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        IUserRepository repo;
+        public HomeController(IUserRepository r)
+        {
+            repo = r;
+        }
+        public ActionResult Index()
+        {
+            return View(repo.GetUsers());
+        }
+
+        public ActionResult Details(int id)
+        {
+            User user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            repo.Create(user);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            User user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            repo.Update(user);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            User user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
